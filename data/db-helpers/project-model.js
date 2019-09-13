4ceedaad8d82b025dff6c11674129f29db365c58
Neha Patel <neha.r.p@gmail.com>
2019-09-13 10:08:54 -0700

    starting to create project-model.js

---
+++ data/db-helpers/project-model.js
@@ -0,0 +1,16 @@
+const db = require("../db-config")
+
+module.exports = {
+    addResource,
+    getResources,
+    addProject,
+    getProjects,
+    addTasks,
+    getTasks
+}
+
+
+
+function getTasks(){
+    //should include project name and description
+}
