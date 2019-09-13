fec1ca4825d9cc6696e1453898b3ad9b4bee091c
Neha Patel <neha.r.p@gmail.com>
2019-09-13 09:33:46 -0700

    in migration, created table for projects

--- data/migrations/20190913092815_create_tables.js
+++ data/migrations/20190913092815_create_tables.js
@@ -1,6 +1,12 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('projects', tbl => {
+      tbl.increments();
+
+      tbl.string('name').notNullable();
+      tbl.string('description')
+      tbl.boolean('completed').defaultTo(false);
+  })
 };
 
 exports.down = function(knex) {
