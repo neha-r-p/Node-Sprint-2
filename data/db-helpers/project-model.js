24e820f64a18a750a9dd7911e8e4d3edbe814008
Neha Patel <neha.r.p@gmail.com>
2019-09-13 10:15:47 -0700

    getResources and getProjects complete in model

--- data/db-helpers/project-model.js
+++ data/db-helpers/project-model.js
@@ -1,41 +1,40 @@
-const db = require("../db-config")
+const db = require("../db-config");
 
 module.exports = {
-    addResource,
-    getResources,
-    addProject,
-    getProjects,
-    addTask,
-    getTasks
-}
+  addResource,
+  getResources,
+  addProject,
+  getProjects,
+  addTask,
+  getTasks
+};
 
-function addResource(resource){
+function addResource(resource) {
   return db("resources")
-  .insert(resource)
-  .then(resource => resource)
+    .insert(resource)
+    .then(resource => resource);
 }
 
-function getResources(){
-
+function getResources() {
+  return db("resources");
 }
 
-function addProject(project){
-    return db("projects")
+function addProject(project) {
+  return db("projects")
     .insert(project)
-    .then(project => project)
+    .then(project => project);
 }
 
-function getProjects(){
-
+function getProjects() {
+  return db("projects");
 }
 
-function addTask(task){
-    return db("tasks")
+function addTask(task) {
+  return db("tasks")
     .insert(task)
-    .then(task => task)
-}
+    .then(task => task);
 }
 
-function getTasks(){
-    //should include project name and description
+function getTasks() {
+  //should include project name and description
 }
