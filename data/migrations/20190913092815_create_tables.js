df5dba534bf78499e7b5badc7c8cff792f8f1c20
Neha Patel <neha.r.p@gmail.com>
2019-09-13 09:39:55 -0700

    made resources and project_resources table schema

--- data/migrations/20190913092815_create_tables.js
+++ data/migrations/20190913092815_create_tables.js
@@ -1,14 +1,39 @@
 exports.up = function(knex) {
   return knex.schema
-  .createTable('projects', tbl => {
+    .createTable("projects", tbl => {
       tbl.increments();
 
-      tbl.string('name').notNullable();
-      tbl.string('description')
-      tbl.boolean('completed').defaultTo(false);
-  })
+      tbl.string("name").notNullable();
+      tbl.string("description");
+      tbl.boolean("completed").defaultTo(false);
+    })
+    .createTable("resources", tbl => {
+      tbl.increments();
+
+      tbl
+        .string("name")
+        .notNullable()
+        .unique();
+    })
+    .createTable("project_resources", tbl => {
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .references("id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("tasks", tbl => {
+      tbl.increments();
+    });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function(knex) {};
